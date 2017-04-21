60d27cae9b36974831210dc7260d08b2da6353da
Simon <noisyboiler@googlemail.com>
2017-04-21 12:04:58 +0100

    wait for subscription helper

--- nameko_wamp/testing/__init__.py
+++ nameko_wamp/testing/__init__.py
@@ -1,1 +1,2 @@
 from . utils import wait_for_registrations  # noqa
+from . utils import wait_for_subscriptions  # noqa
--- nameko_wamp/testing/utils.py
+++ nameko_wamp/testing/utils.py
@@ -1,7 +1,7 @@
 import eventlet
 from wampy.errors import WampyError
 
-from nameko_wamp.extensions import WampCalleeProxy
+from nameko_wamp.extensions import WampCalleeProxy, WampTopicProxy
 
 
 def wait_for_registrations(container, number_of_registrations):
@@ -24,5 +24,29 @@ def wait_for_registrations(container, number_of_registrations):
         while (
             len(session.registration_map.keys())
             < number_of_registrations
+        ):
+            eventlet.sleep()
+
+
+def wait_for_subscriptions(container, number_of_subscriptions):
+    if not container.started:
+        raise WampyError(
+            "Cannot look for registrations unless the service is running"
+        )
+
+    for ext in container.extensions:
+        if type(ext) == WampTopicProxy:
+            break
+    else:
+        raise WampyError(
+            "no clients found subscribing to topics"
+        )
+
+    session = ext.client.session
+
+    with eventlet.Timeout(5):
+        while (
+            len(session.subscription_map.keys())
+            < number_of_subscriptions
         ):
             eventlet.sleep()
